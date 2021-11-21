6407188bfe0cd3fd3d3e95b8b8d7c1d2a6299236
SOUnit <workbookwork7@gmail.com>
2021-11-20 17:22:37 -0800

    add reading jobs with target skill

--- src/main/java/com/spring/pma/controller/JobController.java
+++ src/main/java/com/spring/pma/controller/JobController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.pma.dao.iJobRepository;
 import com.spring.pma.entity.Job;
+import com.spring.pma.entity.Project;
 
 @Controller
 @RequestMapping("/jobs")
@@ -26,6 +28,12 @@ public class JobController {
 		return "jobs/list-jobs";
 	}
 	
-	
+	@GetMapping("/skill/{skill}")
+	public String displayProjectsTests(Model model, @PathVariable("skill") String skill) {
+		System.out.println(skill);
+		List<Job> jobs = jobRepo.getJobsWithTheSkill(skill);
+		model.addAttribute("jobList", jobs);
+		return "jobs/list-jobs";
+	}
 
 }
